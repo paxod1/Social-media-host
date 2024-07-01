4df79f56c10104abc7f8d871461f51e6a1807ad4
paxod1 <156282097+paxod1@users.noreply.github.com>
2024-07-01 20:33:38 +0530

    Update index.js

--- Backends/index.js
+++ Backends/index.js
@@ -88,7 +88,7 @@ io.on('connection', (socket) => {
 });
 
 // Server setup
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
