0e1065d072af879dbf138721045e23b997e49b5c
chiku binoy <chikubinoybinoy@gmail.com>
2024-07-02 11:36:27 +0530

    Commit message

--- Frontends/src/AxiosCreate.jsx
+++ Frontends/src/AxiosCreate.jsx
@@ -1,14 +1,6 @@
 import axios from 'axios';
 
 const SampleUrl = 'https://social-media-host-backends.onrender.com';
-
-// Axios instance with basic configuration
-export const basicRequest = axios.create({
-  baseURL: SampleUrl,
-  timeout: 10000, // Example timeout configuration (adjust as needed)
-});
-
-// Axios instance with token-based authentication
 const persistedLoginData = localStorage.getItem("persist:logindata");
 const loginData = persistedLoginData ? JSON.parse(persistedLoginData) : {};
 const loginInfo = loginData.userlogin ? JSON.parse(loginData.userlogin).LoginInfo[0] : null;
@@ -16,15 +8,15 @@ const AdminloginInfo = loginData.adminLogin ? JSON.parse(loginData.adminLogin).L
 
 const TOKEN = loginInfo ? loginInfo.Token : '';
 const AdminToken = AdminloginInfo ? AdminloginInfo.Token : '';
+export const basicRequest = axios.create({
+  baseURL: SampleUrl
+});
 
 export const TokenRequest = axios.create({
   baseURL: SampleUrl,
-  headers: { Authorization: `Bearer ${TOKEN}` },
-  timeout: 10000, // Example timeout configuration (adjust as needed)
+  headers: { Authorization: `Bearer ${TOKEN}` }
 });
-
 export const AdminTokenRequest = axios.create({
   baseURL: SampleUrl,
-  headers: { Authorization: `Bearer ${AdminToken}` },
-  timeout: 10000, // Example timeout configuration (adjust as needed)
+  headers: { Authorization: `Bearer ${AdminToken}` }
 });
